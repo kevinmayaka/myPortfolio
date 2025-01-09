4d538d5162b336af1ec483f80e77ef1eea2fd3c1
kevin mayaka <kevinabuga3@gmail.com>
2025-01-09 23:56:08 +0300

    adds an annimated background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { Helmet } from "react-helmet"
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
 import "./App.css";
 import Home from "./Home";
 import Navbar from "./Navbar";
@@ -8,6 +12,72 @@ import Skills from "./Skills";
 import ContactMe from "./ContactMe";
 
 function App() {
+    const options = {
+        particles: {
+          number: {
+            value: 80,
+            density: {
+              enable: true,
+              area: 800
+            }
+          },
+          color: {
+            value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0"]
+          },
+          shape: {
+            type: "circle"
+          },
+          opacity: {
+            value: 1
+          },
+          size: {
+            value: { min: 1, max: 8 }
+          },
+          links: {
+            enable: true,
+            distance: 150,
+            color: "#808080",
+            opacity: 0.4,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 5,
+            direction: "none",
+            random: false,
+            straight: false,
+            outModes: "out"
+          }
+        },
+        interactivity: {
+          events: {
+            onHover: {
+              enable: true,
+              mode: "grab"
+            },
+            onClick: {
+              enable: true,
+              mode: "push"
+            }
+          },
+          modes: {
+            grab: {
+              distance: 140,
+              links: {
+                opacity: 1
+              }
+            },
+            push: {
+              quantity: 4
+            }
+          }
+        }
+    };
+    
+    const particlesInit = useCallback(async (engine) => {
+        await loadFull(engine);
+    }, []);
+    
     const location = useLocation();
 
     useEffect(() => {
@@ -19,9 +89,29 @@ function App() {
             window.scrollTo(0, 0); 
         }
     }, [location]);
+    
+
 
     return (
         <div className="App">
+            {/* <Helmet bodyAttributes={{ style: "background-color : #333" }} /> */}
+            <Helmet>
+    <link
+    href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap')"
+    rel="stylesheet"
+  />
+  {/* Set global styles */}
+  <style>
+    {`
+      body {
+        background-color: #333;
+        color: #fff;
+        font-family: 'Poppins', sans-serif;
+      }
+    `}
+  </style>
+</Helmet>
+
             <Navbar />
             <div id="/">
                 <Home />
@@ -35,6 +125,10 @@ function App() {
             <div id="contactme">
                 <ContactMe />
             </div>
+            <div className="particles-background">
+              <Particles options={options} init={particlesInit} />
+            </div>
+
         </div>
     );
 }
